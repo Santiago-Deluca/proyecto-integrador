c668049d50bbcec99d1d039146d31fe8fe986b1f
Guillermo C <guillermoecarranza@gmail.com>
2018-05-14 10:23:24 -0300

    1405

---
+++ footer.php
@@ -0,0 +1,35 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Oswald|PT+Sans" rel="stylesheet">
+  <title>Home</title>
+</head>
+  <body>
+
+  <footer>
+      <div class="navegacion-footer">
+        <div class="contactos">
+          <h4>Telefonos de contactos</h4>
+          <p>Fijo: 011-4701-5997</p>
+          <p>Celular: 155-4545-2222</p>
+          <h4>Mail:</h4>
+          <p>info@comamosjuntos.com.ar</p>
+        </div>
+        <nav  class="redes-sociales">
+          <a href="#"><i class="fa fa-facebook-f"></i></a>
+          <a href="#"><i class="fa fa-twitter-square"></i></a>
+          <a href="#"><i class="fa fa-instagram"></i></a>
+          <a href="#"><i class="fa fa-pinterest-square"></i></a>
+          <a href="#"><i class="fa fa-youtube"></i></a>
+       </nav>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ home.php
@@ -0,0 +1,77 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estilos.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Oswald|PT+Sans" rel="stylesheet">
+  <title>Home</title>
+</head>
+
+  <body>
+
+    <header class="site-header">
+      <div class="hero">
+        <div class="contenido-header">
+          <nav  class="redes-sociales">
+            <a href="#"><i class="fa fa-facebook-f"></i></a>
+            <a href="#"><i class="fa fa-twitter-square"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+            <a href="#"><i class="fa fa-pinterest-square"></i></a>
+            <a href="#"><i class="fa fa-youtube"></i></a>
+          </nav>
+        </div>
+        <div class="informacion">
+          <h1 class="nombre-sitio">Nombre del sitio</h1>
+          <p class="slogan">Basta de comer siempre lo mismo ! </p>
+        </div>
+      </header>
+
+    <div class="barra">
+      <div class="contenedor clearfix">
+        <div class="logo">
+          <img src=" " alt="logo">
+        </div>
+        <div class="menu-movil">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <nav class="navegacion-principal clearfix">
+          <a href="home.php">Home</a>
+          <a href="login.php">Login</a>
+          <a href="faq.php">Preguntas frecuentes</a>
+          <a href="registro.php">Registro</a>
+        </nav>
+      </div>
+    </div>
+
+
+<section>
+  <div class="explicacion">
+      <p>La web 2.0 cambió el modo en el que las personas experimentan la realidad. En particular, dio inicio a una revolución en la que cada individuo tiene la posibilidad de aportar sin intermediarios al cuerpo de conocimiento de la humanidad, a la vez que puede acceder de forma instantánea a ese conocimiento inabarcable.</p>
+    <p>Podemos compartir la experiencia de lo que vemos, la experiencia de lo que oímos, pero ¿dónde queda el sabor?</p>
+    <p>Todos tenemos una receta. La que la abuela te mostró, y te lleva a esas mañanas de sol. La que siempre hace sonreir a tus chicos. La que aparece siempre en las historias de tus amigos.</p>
+    <p>Yo quiero conocer la tuya, la de él, la de todos. Compartamos el sabor.</p>
+    <br>
+    <h2>Como funciona</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <br>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</section>
+
+
+<?php
+include ("footer.php");
+?>
+
+
+  </body>
+</html>
