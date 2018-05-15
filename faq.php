1795384437c91d8c97aa5bd0168075f50f22f1a3
juanaorlandini <38474414+juanaorlandini@users.noreply.github.com>
2018-05-15 17:13:16 -0300

    prueba

--- faq.php
+++ faq.php
@@ -1,96 +1,96 @@
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/estilos.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Oswald|PT+Sans" rel="stylesheet">
-  <title>Home</title>
-</head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Oswald|PT+Sans" rel="stylesheet">
+    <title>Home</title>
+  </head>
   <body>
- <h2> Preguntas Frecuentes</h2>
+    <?php
+    include ("head.php");
+    ?>
+    <section class="cuerpo">    
+      <h2> Preguntas Frecuentes</h2>
+      <section>
+     		<div class="faq">
+     			<h2>Preguntas frecuentes</h2>
+      			<ol>
+       				<li>¿Donde estamos?</li>
+       				<p> Nos encontramos ubicados en el Barrio de Belgrano, entre las Av. Monroe y Av. del Libertador Cap. Fed.</p>
+       				<li>¿Hasta donde llegamos con el Delivery?</li>
+       				<p>Llegamos a un radio de tres (3) kilometros</p>
+       				<li>¿Quien envia nuestros pedidos?</li>
+       				<p>Tenemos varia motos propias para asegurarle al cliente una entrega rápida y segura</p>
+       				<li>¿Por qué elegirnos?</li>
+       				<p>Por la excelente relacion precio y calidad con una entrega rápida </p>
+       				<li>¿Por qué registrarme?</li>
+       				<p>Para que disfrutes de todos nuestras promociones especiales, cupones de descuentos, obsequios y mucho más</p>
+       			</ol>
+            <div class="volver">
+              <a href="home.php">Volver</a>
+            </div>
+       		</div>
+       	</section>
 
-
- <section>
- 		<div class="faq">
- 			<h2>Preguntas frecuentes</h2>
- 			<ol>
- 				<li>¿Donde estamos?</li>
- 				<p> Nos encontramos ubicados en el Barrio de Belgrano, entre las Av. Monroe y Av. del Libertador Cap. Fed.</p>
- 				<li>¿Hasta donde llegamos con el Delivery?</li>
- 				<p>Llegamos a un radio de tres (3) kilometros</p>
- 				<li>¿Quien envia nuestros pedidos?</li>
- 				<p>Tenemos varia motos propias para asegurarle al cliente una entrega rápida y segura</p>
- 				<li>¿Por qué elegirnos?</li>
- 				<p>Por la excelente relacion precio y calidad con una entrega rápida </p>
- 				<li>¿Por qué registrarme?</li>
- 				<p>Para que disfrutes de todos nuestras promociones especiales, cupones de descuentos, obsequios y mucho más</p>
- 			</ol>
-      <div class="volver">
-        <a href="home.php">Volver</a>
-      </div>
- 		</div>
- 	</section>
-
-    <div class="col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 col-xs-12" id="faq">
-        <div class="block">
-           <h3><a href="#" class="default-color no-underline"> ¿Que es Comemos Juntos?</a></h3>
-           <p> Es una plataforma que te permite tener acceso a comida casera.
-        Tu puedes ser un Chef, con sólo la cocina de tu casa y la app de Comemos Juntos ? (link a la tienda) para publicar tus platos, ya tienes tu restaurante.
-        Todos somos Gourmets: nos gusta comer casero y saludable cada día. Con tu Smartphone, busca los platos que más te gusten cerca de ti.</p>
-         </div>
-        <div class="block">
+        <div class="col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 col-xs-12" id="faq">
+          <div class="block">
+            <h3><a href="#" class="default-color no-underline"> ¿Que es Comemos Juntos?</a></h3>
+            <p> Es una plataforma que te permite tener acceso a comida casera.
+          Tu puedes ser un Chef, con sólo la cocina de tu casa y la app de Comemos Juntos ? (link a la tienda) para publicar tus platos, ya tienes tu restaurante.
+          Todos somos Gourmets: nos gusta comer casero y saludable cada día. Con tu Smartphone, busca los platos que más te gusten cerca de ti.</p>
+          </div>
+          <div class="block">
             <h3><a href="#" class="default-color no-underline"> ¿Quiénes pueden participar?</a></h3>
             <p style="display: none"> Todos aquellos que quieran compartir sus platos y raciones de comida. Cualquiera con la cocina de su casa y un Smartphone puede hacerlo. Descarga la app (link), regístrate y empieza compartir y vender tu comida. </p>
-        </div>
-        <div class="block">
-            <h3><a href="#" class="default-color no-underline"> ¿Quiénes son los Gourmets? </a></h3>
-            <p style="display: none"> Los Gourmets somos cualquiera que quiere comer algún plato/ración de los Chefs. Tu puedes serlo. Para eso debes descargarte la app. (link) </p>
-        </div>
+          </div>
+          <div class="block">
+              <h3><a href="#" class="default-color no-underline"> ¿Quiénes son los Gourmets? </a></h3>
+              <p style="display: none"> Los Gourmets somos cualquiera que quiere comer algún plato/ración de los Chefs. Tu puedes serlo. Para eso debes descargarte la app. (link) </p>
+          </div>
 
-        <div class="block">
+          <div class="block">
             <h3><a href="#" class="default-color no-underline"> ¿Dónde se come?</a></h3>
-           <p style="display: none"> Solo es question de ponerse de acuerdo dia , hora y elegir punto de encuentro.</p>
-        </div>
+            <p style="display: none"> Solo es question de ponerse de acuerdo dia , hora y elegir punto de encuentro.</p>
+          </div>
 
-        <div class="block">
+          <div class="block">
             <h3><a href="#" class="default-color no-underline"> ¿Cómo se paga?</a></h3>
             <p style="display: none"> Toda la transacción se hace por trueque cada uno acuerda llevar la comida que acordaron previamente.</p>
-        </div>
+          </div>
 
-        <div class="block">
+          <div class="block">
             <h3><a href="#" class="default-color no-underline"> Reservas</a></h3>
             <p style="display: none"> El sistema de reservas está pensado para que todos los que participan se comprometan a cumplir la cita. Con el fin de garantizar la mayor fluidez, se deberá aprobar la solicitud de reserva del Gourmet.
-      Se puede cancelar la reserva sin coste alguno siempre que se haga antes de dos horas del encuentro.
-      Gourmet pide al Chef cambios a tu gusto del plato que quieres en el momento de la reserva! Por ejemplo: no añadas sal a la comida!</p>
-        </div>
+        Se puede cancelar la reserva sin coste alguno siempre que se haga antes de dos horas del encuentro.
+        Gourmet pide al Chef cambios a tu gusto del plato que quieres en el momento de la reserva! Por ejemplo: no añadas sal a la comida!</p>
+          </div>
 
-        <div class="block">
+          <div class="block">
             <h3><a href="#" class="default-color no-underline"> Cancelación de reservas. </a></h3>
             <p style="display: none"> Las reservas se pueden cancelar sin coste si se hacen antes de una hora de la cita.
-    Los Gourmets si cancelan dentro de la hora antes de la cita pagan el coste de la reserva. El Chef recibirá el importe total de la reserva si su Gourmet cancela demasiado tarde, y el Gourmet recibirá el importe de vuelta si el Chef cancela dentro de la ultima hora.
-    Si el Chef cancela dentro de la ultima hora antes de la cita le será cobrada una penalización de 1,30€.
-    Todas las reservas canceladas con tiempo, la devolución es inmediata.</p>
-        </div>
+          Los Gourmets si cancelan dentro de la hora antes de la cita pagan el coste de la reserva. El Chef recibirá el importe total de la reserva si su Gourmet cancela demasiado tarde, y el Gourmet recibirá el importe de vuelta si el Chef cancela dentro de la ultima hora.
+          Si el Chef cancela dentro de la ultima hora antes de la cita le será cobrada una penalización de 1,30€.
+          Todas las reservas canceladas con tiempo, la devolución es inmediata.</p>
+          </div>
 
-        <div class="block">
+          <div class="block">
             <h3><a href="#" class="default-color no-underline">  Valoraciones </a></h3>
             <p style="display: none"> Tanto Chefs como Gourmets se valoran mutuamente. Gustoo es 100% colaborativo, tu opinión es muy importante.
     Es tan fácil como darle al (icono me gusta).
     Si eres de los que te gusta añadir fotos y comentarios, hazlo, tu comunidad Gustoo te lo agradecerá.</p>
-        </div>
+          </div>
 
-        <div class="block">
+          <div class="block">
             <h3><a href="#" class="default-color no-underline"> Pide un deseo… </a></h3>
             <p style="display: none"> Es un campo de búsqueda y tu Aladín particular. Escribe el plato de lo que te apetece comer, Gustoo lo buscará de entre los Chefs cercanos. En caso que no se encuentre lo que te gusta, se mandará una petición de plato a los Chefs de la zona. Así siempre comerás lo que te apetezca.</p>
+          </div>
         </div>
-    </div>
-
-<?php
-    include ("footer.php");
-?>
-
+      </section>
+    <?php
+        include ("footer.php");
+    ?>
   </body>
 </html>
--- login.php
+++ login.php
@@ -1,56 +1,58 @@
-
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/estilos.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Oswald|PT+Sans" rel="stylesheet">
-  <title>Home</title>
-</head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Oswald|PT+Sans" rel="stylesheet">
+    <title>Home</title>
+  </head>
   <body>
-    <div class="panel-body">
-        <h2 class="text-center">Inicia sesión</h2>
-        <button class="btn btn-block btn-facebook" ng-click="login.authenticate('facebook')">
-        <i class="fa fa-facebook"></i> Conéctate con Facebook
-        </button>
-        <br>
-        <div class="signup-or-separator">
+    <?php
+      include ("head.php");
+    ?>
+    <section class="cuerpo">
+      <div class="panel-body">
+          <h2 class="text-center">Inicia sesión</h2>
+          <button class="btn btn-block btn-facebook" ng-click="login.authenticate('facebook')">
+            <i class="fa fa-facebook"></i> Conéctate con Facebook
+          </button>
+          <br>
+          <div class="signup-or-separator">
             <h6 class="text">o</h6>
             <hr>
+          </div>
+
+          <form method="post" ng-submit="login.login()" name="loginForm" class="ng-pristine ng-invalid ng-invalid-required">
+            <div class="form-group has-feedback">
+              <input class="form-control input-lg ng-pristine ng-invalid ng-invalid-required ng-touched" type="text" name="email" ng-model="login.email" placeholder="Introduce tu email" required="" autofocus="">
+              <span class="fa fa-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+              <input class="form-control input-lg ng-pristine ng-untouched ng-invalid ng-invalid-required" type="password" name="password" ng-model="login.password" placeholder="Y tu contraseña" required="">
+              <span class="fa fa-lock form-control-feedback"></span>
+            </div>
+            <button type="submit" ng-disabled="loginForm.$invalid" class="btn btn-lg btn-block btn-primary" disabled="disabled">Inicia sesión</button>
+            <br>
+            <p class="text-center">
+              <a href="/forgot">¿Has olvidado tu contraseña?</a>
+            </p>
+            <p class="text-center text-muted">
+              <small>¿Aún no estás registrado? <a href="/signup">Abre tu cuenta aquí.</a></small>
+              <!--<button class="btn btn-block btn-google-plus" ng-click="login.authenticate('google')">
+              <span class="fa fa-google-plus"></span> Sign in with Google
+              </button>-->
+            </p>
+          </form>
         </div>
 
-        <form method="post" ng-submit="login.login()" name="loginForm" class="ng-pristine ng-invalid ng-invalid-required">
-        <div class="form-group has-feedback">
-            <input class="form-control input-lg ng-pristine ng-invalid ng-invalid-required ng-touched" type="text" name="email" ng-model="login.email" placeholder="Introduce tu email" required="" autofocus="">
-            <span class="fa fa-envelope form-control-feedback"></span>
+        <div class="volver">
+          <a href="home.php">Volver</a>
         </div>
-        <div class="form-group has-feedback">
-            <input class="form-control input-lg ng-pristine ng-untouched ng-invalid ng-invalid-required" type="password" name="password" ng-model="login.password" placeholder="Y tu contraseña" required="">
-            <span class="fa fa-lock form-control-feedback"></span>
-        </div>
-          <button type="submit" ng-disabled="loginForm.$invalid" class="btn btn-lg btn-block btn-primary" disabled="disabled">Inicia sesión</button>
-          <br>
-          <p class="text-center">
-          <a href="/forgot">¿Has olvidado tu contraseña?</a>
-          </p>
-          <p class="text-center text-muted">
-          <small>¿Aún no estás registrado? <a href="/signup">Abre tu cuenta aquí.</a></small>
-        <!--<button class="btn btn-block btn-google-plus" ng-click="login.authenticate('google')">
-          <span class="fa fa-google-plus"></span> Sign in with Google
-        </button>-->
-      </p></form></div>
-
-      <div class="volver">
-        <a href="home.php">Volver</a>
-      </div>
-
+      </section>
       <?php
           include ("footer.php");
       ?>
-
-
   </body>
 </html>
