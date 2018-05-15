eaa63d1c542fc0f6c42875b9993db47d99b78015
Guillermo C <guillermoecarranza@gmail.com>
2018-05-15 18:13:33 -0300

    Revision footer head

--- footer.php
+++ footer.php
@@ -1,13 +1,5 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilos.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Oswald|PT+Sans" rel="stylesheet">
-    <title>Home</title>
-  </head>
   <body>
     <footer>
       <div class="pie_de_pagina">
