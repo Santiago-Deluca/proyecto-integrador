f6f241c852acabf6ecf56e08bf67d13fcc0d27dc
Guillermo C <guillermoecarranza@gmail.com>
2018-05-17 14:54:46 -0300

    Actualización del FAQ

--- faq.php
+++ faq.php
@@ -18,16 +18,16 @@
      		<div class="panelbig">
      			<h2>Preguntas frecuentes</h2>
     			<ul>
-     				<li>¿Donde estamos?</li>
-     				<p> Nos encontramos ubicados en el Barrio de Belgrano, entre las Av. Monroe y Av. del Libertador Cap. Fed.</p>
-     				<li>¿Hasta donde llegamos con el Delivery?</li>
-     				<p>Llegamos a un radio de tres (3) kilometros</p>
-     				<li>¿Quien envia nuestros pedidos?</li>
-     				<p>Tenemos varia motos propias para asegurarle al cliente una entrega rápida y segura</p>
-     				<li>¿Por qué elegirnos?</li>
-     				<p>Por la excelente relacion precio y calidad con una entrega rápida </p>
-     				<li>¿Por qué registrarme?</li>
-     				<p>Para que disfrutes de todos nuestras promociones especiales, cupones de descuentos, obsequios y mucho más</p>
+     				<li>¿Quién puede participar?</li>
+     				<p>Alumnos, docentes, empleados y cualquier otra persona que pueda acceder a Digital House. Si, podés traer el tupper de mamá.</p>
+     				<li>¿Puedo pagar por la comida?</li>
+     				<p>Si bien la aplicación sólo se encarga de conectarte y luego el trato es entre personas, el espíritu de la aplicación es de intercambio de comida y experiencias.</p>
+     				<li>¿Tengo que sentarme a comer con la persona como en las fotos?</li>
+     				<p>La aplicación te pondrá en contacto con tu match y ustedes arreglarán la logística del intercambio, sea almorzar juntos, intercambiar la comida en el recreo, cualquier otra opción.</p>
+     				<li>¿Qué pasa si yo llevo algo enorme, super rico y la otra persona trae algo chiquito y feo?</li>
+     				<p>Las porciones deberían conversarse entre los usuarios. Si tuvieses una mala experiencia, te invito a que hagas un segundo intercambio conmigo. Cocino abundante y lleno de calorías.</p>
+     				<li>Soy celíaco, ¿puedo utilizar la plataforma?</li>
+     				<p>Si, la aplicación permite marcar tus recetas como "aptas para celíacos", y filtrar las recetas de los usuarios con el mismo criterio.</p>
      			</ul>
         </div>    
       </div>
